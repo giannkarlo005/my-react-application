8c1ed8d05614cb4c5e13a17795a1bf48217882d0
GIANN-PC\Giann <giannkarlo_05@outlook.com>
2023-02-02 20:34:29 +0800

    Remove warnings

--- src/components/about.js
+++ src/components/about.js
@@ -29,7 +29,6 @@ export const About = () => {
                     quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
                     unde dolor?
                 </p>
-                <a href="#" className="btn">read more</a>
             </article>
         </div>
     </section>
